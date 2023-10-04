90f4adeddace4d8cfe63dba87c9b9ef9616a8423
USUARIO <USUARIO@DESKTOP-N0DKPVM>
2023-10-04 01:06:46 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divisores de Juan David:");
+            Console.WriteLine("=============================");
+
+            for (int i = 1; i <= 1000; i++) {
+
+                if (i % 3 == 0) {
+
+                    Console.WriteLine("Divisor de Juan David " + (i / 3) + ": " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
